757688e46239af41c891e4d71f98c0b8bb889b63
Fulop-Krisztian <50krissssz22@gmail.com>
2024-04-28 16:53:27 +0200

    version 0.2: bar2, speed thrust and resistance

--- PID.js
+++ PID.js
@@ -39,10 +39,13 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 // the value the system should aim for 
 var setPoint = 100;
 // the current value of the system
-var current = 3000;
+var current = 0;
+var speed = 0;
+var gravity = -10; // units/interval/interval
 var prevcurr = -1;
 // how far we are from where we want to be
 // let error: number = 0
+var resistance = 0.005;
 // used for the derivative calculation
 var preverror = 0;
 // derivative
@@ -56,13 +59,13 @@ var intacc = 0;
 // the interval between out measurements. Could use the actual process time in the future.
 var interval = 1;
 // renders a bar. In the console no less. 
-function renderbar(current) {
+function renderbar(curr, sp, width) {
     // we should render the setpoint at 75% of the length. 
-    var setPointLinePos = 0.50;
+    var setPointLinePos = 0.75;
     // The length of the decor on the bar
     // it will look something like this:
     //<#############---|--->23.00/123.33
-    var decorlength = current.toString.length + setPoint.toString.length + 3; // 3 for the < > /
+    var decorlength = current.toString.length + sp.toString.length + 3; // 3 for the < > /
     // The length of the bar in characters. Currently it's the length of the console -10 chars, or 80 chars if the length is undefined
     var barlength = process.stdout.columns - decorlength - 10 || 80;
     var bar = '';
@@ -72,7 +75,7 @@ function renderbar(current) {
     const ZFR = 0 // zoomfromright
      */
     for (var index = 0; index < barlength; index++) {
-        if (current / setPoint * (barlength * setPointLinePos) > index) {
+        if (curr / sp * (barlength * setPointLinePos) > index) {
             bar += '#';
         }
         else if (index === Math.trunc(setPointLinePos * barlength)) {
@@ -86,52 +89,99 @@ function renderbar(current) {
     // Where the magic happens
     process.stdout.clearLine(0);
     process.stdout.cursorTo(0);
-    process.stdout.write("<".concat(bar, ">").concat(Math.trunc(current), "/").concat(Math.trunc(setPoint)));
+    process.stdout.write("<".concat(bar, ">").concat(Math.trunc(curr), "/").concat(Math.trunc(sp)));
 }
-function PID(sp, cur, ks, kp, kd, ki, time) {
+function renderbar2(crnt, sp, width) {
+    // multi character support to be implemented
+    function renderrow(character, width) {
+        var stringlength = character.length;
+        var row = '';
+        for (var index = 0; index < width; index += stringlength) {
+            row += character;
+        }
+        row += '\n';
+        return row;
+    }
+    // we should render the setpoint at 75% of the length. 
+    var setPointLinePos = 0.50;
+    // decoration in the vertical direction
+    var decorheight = 1;
+    var decorlength = 0;
+    var barheight = (Math.trunc((process.stdout.rows - decorheight) * 0.9)) || 30;
+    var barlength = width; //(process.stdout.columns - decorlength - 10) || 80
+    var bar = '';
+    bar += "".concat(crnt, "/").concat(sp, "\n");
+    renderrow('-', barlength);
+    for (var index = 0; index < barheight; index++) {
+        if ((crnt / sp) * (barheight * setPointLinePos) < index) {
+            bar += renderrow('#', barlength);
+        }
+        else if (index === Math.trunc(setPointLinePos * barheight)) {
+            bar += renderrow('—', barlength);
+        }
+        else {
+            bar += renderrow(' ', barlength);
+        }
+    }
+    console.log(bar + '\n');
+    return bar;
+}
+function rendergraph() {
+}
+// PID takes in variables about the system and returns the current predicted drive we should have in the system
+function PID(sp, cur, kp, kd, ki, time) {
+    function calcDerivative(error, perror, interval) {
+        var derivative = (error - perror) / interval;
+        preverror = error;
+        return derivative;
+    }
+    function calcIntegral(error, acc, interval) {
+        var integral = acc + (error * interval);
+        intacc = integral;
+        return integral;
+    }
     // kp, kd, and ki are the multipliers for each of the controls. (higher means higher priority, could easily unbalance the system) 
     // ks is a strength multiplier
     var err = sp - cur;
     var drive = kp * err;
     var derivativeDrive = kd * calcDerivative(err, preverror, time);
     var integralDrive = ki * calcIntegral(err, intacc, time);
-    //console.log(drive,derivativeDrive,integralDrive)
+    console.log(drive, derivativeDrive, integralDrive);
     var output = drive + derivativeDrive + integralDrive; // proportional + derivative + integral drive. numbers should be tuned for best results
     //console.log(current)
     //console.log(output)
-    current = current + (output * ks);
+    // current = current + (output * ks)
     //console.log(current)
-    return current;
-}
-function calcDerivative(error, perror, interval) {
-    var derivative = (error - perror) / interval;
-    preverror = error;
-    return derivative;
-}
-function calcIntegral(error, acc, interval) {
-    var integral = acc + (error * interval);
-    intacc = integral;
-    return integral;
+    // In the future this should be used as just a drive for an actual thruster or something, right now it's a linear function from output to actual effect. (perfect controls)
+    // Perfect controls are not realistic, but realism requires physics
+    return output;
 }
 function main() {
     return __awaiter(this, void 0, void 0, function () {
-        var index, value;
+        var index, drive;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
                     index = 0;
                     _a.label = 1;
                 case 1:
-                    if (!(index < 1000)) return [3 /*break*/, 4];
-                    return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, 20); })];
+                    if (!(index < 100)) return [3 /*break*/, 4];
+                    //sleep
+                    return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, 16); })];
                 case 2:
+                    //sleep
                     _a.sent();
-                    value = PID(setPoint, current, 0.5, 0.5, 0.5, 0.3, 1);
-                    renderbar(value);
-                    setPoint = setPoint + index / 2;
-                    if (index % 129 === 0) {
-                        setPoint = setPoint / 3 - setPoint / 2 + index;
-                    }
+                    drive = PID(setPoint, current, 1, 0.1, 0.1, 1);
+                    // The drive and gravity are added to the speed
+                    speed = speed + drive * 0.2 + (interval * gravity);
+                    // drag is then calculated so the number doesn't get wacky
+                    speed = speed - (resistance * (speed * speed));
+                    current += speed;
+                    console.log('Node PID');
+                    console.log("Thrust: ".concat(drive));
+                    console.log("Speed: ".concat(speed));
+                    renderbar(current, setPoint, 3);
+                    console.log('\n');
                     _a.label = 3;
                 case 3:
                     index++;
